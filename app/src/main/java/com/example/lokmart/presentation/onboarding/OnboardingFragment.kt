06dc75a5f6dd51e2c131d98f8187d09a04023ed5
Nosirjonov Qosimjon <nosirjonov.dev@gmail.com>
2025-10-18 22:29:09 +0500

    Onboarding

--- app/src/main/java/com/example/lokmart/presentation/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/example/lokmart/presentation/onboarding/OnboardingFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.lokmart.R
 import com.example.lokmart.databinding.FragmentOnboardingBinding
+import com.zhpan.indicator.enums.IndicatorSlideMode
+import com.zhpan.indicator.enums.IndicatorStyle
 
 class OnboardingFragment : Fragment() {
 
     private lateinit var binding : FragmentOnboardingBinding
+    private val adapter = OnboardingAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -19,5 +23,26 @@ class OnboardingFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() = with(binding) {
+        pager.adapter = adapter
+
+        indicator.apply {
+            val normalColor = ContextCompat.getColor(requireContext(), R.color.indicator_unchecked)
+            val checkedColor = ContextCompat.getColor(requireContext(), R.color.indicator_checked)
+            setSliderColor(normalColor, checkedColor)
+            setSliderWidth(resources.getDimension(R.dimen.dp_15))
+            setSliderHeight(resources.getDimension(R.dimen.dp_8))
+            setSlideMode(IndicatorSlideMode.WORM)
+            setIndicatorStyle(IndicatorStyle.CIRCLE)
+            setPageSize(adapter.itemCount)
+            notifyDataChanged()
+        }
+    }
 
 }
