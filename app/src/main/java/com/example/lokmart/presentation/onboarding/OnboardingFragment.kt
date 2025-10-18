f99d0e302f80bef19bee25231c9a4a2d962df9a6
Nosirjonov Qosimjon <nosirjonov.dev@gmail.com>
2025-10-18 20:20:53 +0500

    Onboarding

---
+++ app/src/main/java/com/example/lokmart/presentation/onboarding/OnboardingFragment.kt
@@ -0,0 +1,20 @@
+package com.example.lokmart.presentation.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lokmart.R
+
+class OnboardingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    }
+
+
+}
